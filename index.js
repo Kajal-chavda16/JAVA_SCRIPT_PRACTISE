6f2b45fff96c0cb9d60160a0fc8f44b96d011e7a
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-06-10 19:12:43 +0530

    Advance JS =promises , callback hell , aync await ,error handling

--- index.js
+++ index.js
@@ -1,55 +1,110 @@
-//* =======================================
-//*  How JavaScript Works?
+//* ---------------------------------------------------
+//*  First-Class Function - it's just a concept
+//* ---------------------------------------------------
+// A language feature where functions are treated as first-class citizens.
+// Functions can be assigned to variables, passed as arguments to other functions, and returned from other functions.
+//?   A "first-class function" means that functions can be treated as values, assigned to variables, and passed around as arguments.
+
+// // Function declaration
+// function sayHello(name) {
+//   return "Hello, " + name + "!";
+// }
+
+// // // Assigning the function to a variable
+// var greetFunction = sayHello;
+
+// // // Using the variable as a function
+// console.log(greetFunction("Thapa"));
+
+//* -------------------------------
+//*  Higher-Order Functions:
+//* -------------------------------
+//? Definition: A higher-order function is a function that takes one or more functions as arguments or returns a function as a result.
+
+//* -------------------------------
+//*  Callback Functions:
+//* -------------------------------
+//? Definition: A callback function is a function passed as an argument to another function and is executed after the completion of a task.
+
+//* Here is the example ✅
+// Callback function
+// function processUserInput(name, greetUser) {
+//   console.log("Received input: " + name);
+//   greetUser(name);
+// }
+
+// // Function to be used as a callback
+// function greetUser(name) {
+//   console.log(`Hello! ${name}`);
+// }
+
+// processUserInput("Vinod", greetUser);
+
+// processUserInput is a higher-order function because it takes another function (callback) as an argument.
+// greetUser is a callback function because it's passed as an argument to processUserInput and gets executed after the completion of the main task
+
+//* -------------------------------
+//*  Closure:
+//* -------------------------------
+
+//? A closure is created when an inner function has access to the variables of its outer function, even after the outer function has finished executing.
+
+// function multiplier(factor) {
+//   return function (number) {
+//     console.log(number, factor);
+//     return number * factor;
+//   };
+// }
+
+// const double = multiplier(2);
+// console.log(double(5));
+
+//* also one more example
+// function outerFunction() {
+//   var outerVariable = "I'm from outer";
+
+//   function innerFunction() {
+//     console.log(outerVariable);
+//   }
+
+//   return innerFunction;
+// }
+
+// var closureFunction = outerFunction();
+// closureFunction(); // Outputs: "I'm from outer"
+
+// Here when we are calling the closureFunction() it is actually going to call the innerFunction() but in our executionContext the outerFunction is already popped  out. Still we can access the outerVaribale value and how we are able to access it, is what closure all about ?
+
+// We just need to know that an inner function has always access to the variables of the outer (parent) function. That's it.
+
+// Why is it like that? It's because the variable object of the parent function stays in memory even after the function returns, and our inner function has access to this variable object through the scope chain.
+
+// It might be beneficial to clarify that the call stack is simply the execution order and it is not the same thing as the computer's memory, which is where the variables are held.  So even though the function was removed from the call stack, its variables are still maintained in memory until they are no longer needed and garbage collection comes and removes them.
+
 //* ===================================
+//* Interview Question:
+//* ===================================
+//! Write a program to perform mathematical operations using callback functions and two variables in JavaScript.
 
-// https://www.jointjs.com/demos/abstract-syntax-tree
+//? Instructions:
+//? Define a higher-order function called mathOperation that takes three arguments: x, y, and operation.
+//? Implement two callback functions:
+//? add: Takes two numbers x and y and returns their sum.
+//? sub: Takes two numbers x and y and returns the result of subtracting x from y.
+//? Use the mathOperation function to perform addition and subtraction operations on two variables a and b.
+//? Display the results of the operations.
 
-//! 1: Parsing Phase
-//* 1. Lexical analysis
-//?Lexical analyzer, also known as a lexer, is the first step in the process of compiling a JavaScript program. It breaks the program down into tokens, which are the basic building blocks of the language.
+const mathOperation = (a, b, operation) => {
+  return operation(a, b);
+};
 
-//* 2. Syntax analysis
-//? Takes the stream of tokens from lexical analysis and checks them for correct syntax. If the syntax is correct, syntax analysis generates a tree-like structure called a parse tree or abstract syntax tree (AST). The AST represents the hierarchical structure of the program.
+const add = (a, b) => {
+  return a + b;
+};
 
-//* 2. Compilation (JIT - Just-In-Time Compilation):
-//? After the AST is created, the JavaScript engine typically goes through a compilation phase. In modern engines, like V8 in Chrome, SpiderMonkey in Firefox, or JavaScriptCore in Safari, this compilation often involves a combination of two approaches:
+const sub = (a, b) => {
+  return b - a;
+};
 
-//? Parse and Compile: The engine parses the code and compiles it into an intermediate form, such as bytecode or machine code.
-
-//? Just-In-Time Compilation (JIT): Some engines use JIT compilation, where the intermediate code is compiled just before execution. This allows the engine to optimize the code based on runtime information, improving performance.
-
-//* 3. Execution:
-//? Once the code is compiled, the JavaScript engine executes it. During execution, the engine creates execution contexts, manages the scope chain, handles variable assignments, and calls functions.
-
-//? The execution context consists of two phases: the creation phase (where variables and functions are hoisted) and the execution phase (where the code is actually run).
-
-//? The JavaScript engine uses a call stack to keep track of the execution context. When a function is called, a new frame is added to the stack, and when the function completes, its frame is removed (LIFO - Last In, First Out).
-
-//* More on inside execution phase
-//! Call Stack
-//? In order to manage the execution contexts, the JavaScript engine uses a call stack.
-//? The call stack is a data structure that keeps track of the currently executing functions in a program. It operates on the Last In, First Out (LIFO) principle, meaning that the last function added to the stack is the first one to be executed and completed.
-//! Heap Memory:
-//? The heap memory is where dynamically allocated memory resides. This is where objects, closures, and other dynamically allocated data are stored. While the call stack manages the flow of execution and function contexts, the heap memory holds data that is referenced by these execution contexts.
-
-//! Bonus We will see in PPT with animation
-//* Here's a basic overview of how the call stack works:
-
-//? During the execution of a JavaScript program,
-
-//? The JavaScript engine goes through the creation phase before executing any code. During this phase, it sets up the global execution context. The global execution context is the first one to be created and pushed onto the call stack. This happens when the JavaScript engine starts executing the code.
-
-//todo  Key activities during the creation phase include:
-
-//? Creating the global object (window in browsers, global in Node.js).
-//? Setting up the this reference.
-//? Creating the outer environment reference (which is null for the global context).
-//? Creating the variable environment and allocating memory for global variables and functions.
-
-//* Before executing our code, JavaScript engine scans the code and creates a property for each variable or function in the code. For variable, It reserves space for them in memory and sets an initial value of undefined, and for functions it also reserves space but sets an initial value as a reference to the actual function in memory. That's why we can call a function, but if we try to access a variable, we will get undefined.
-
-//? Setting up the scope chain, which initially contains only the global scope.
-
-//! Execution Phase:
-//? After the creation phase, the actual code execution takes place. This is when the JavaScript engine goes through the code line by line.
-//? Variables are assigned their values, functions are executed, and the program's logic is carried out.
+console.log(mathOperation(5, 15, add));
+console.log(mathOperation(5, 15, sub));
