56bd67c1ea1cfd8d31f00f2d1aaccdb009cffe10
Kajal-chavda16 <157488274+Kajal-chavda16@users.noreply.github.com>
2024-06-11 22:23:28 +0530

    weather app

--- index.js
+++ index.js
@@ -1,110 +1,84 @@
-//* ---------------------------------------------------
-//*  First-Class Function - it's just a concept
-//* ---------------------------------------------------
-// A language feature where functions are treated as first-class citizens.
-// Functions can be assigned to variables, passed as arguments to other functions, and returned from other functions.
-//?   A "first-class function" means that functions can be treated as values, assigned to variables, and passed around as arguments.
 
-// // Function declaration
-// function sayHello(name) {
-//   return "Hello, " + name + "!";
-// }
+let cityName = document.querySelector(".weather_city");
+let dateTime = document.querySelector(".weather_date_time");
+let w_forecast = document.querySelector(".weather_forecast");
+let w_icon = document.querySelector(".weather_icon");
+let w_temperature = document.querySelector(".weather_temperature");
+let w_minTem = document.querySelector(".weather_min");
+let w_maxTem = document.querySelector(".weather_max");
 
-// // // Assigning the function to a variable
-// var greetFunction = sayHello;
+let w_feelsLike = document.querySelector(".weather_feelsLike");
+let w_humidity = document.querySelector(".weather_humidity");
+let w_wind = document.querySelector(".weather_wind");
+let w_pressure = document.querySelector(".weather_pressure");
 
-// // // Using the variable as a function
-// console.log(greetFunction("Thapa"));
+let citySearch = document.querySelector(".weather_search");
 
-//* -------------------------------
-//*  Higher-Order Functions:
-//* -------------------------------
-//? Definition: A higher-order function is a function that takes one or more functions as arguments or returns a function as a result.
-
-//* -------------------------------
-//*  Callback Functions:
-//* -------------------------------
-//? Definition: A callback function is a function passed as an argument to another function and is executed after the completion of a task.
-
-//* Here is the example ✅
-// Callback function
-// function processUserInput(name, greetUser) {
-//   console.log("Received input: " + name);
-//   greetUser(name);
-// }
-
-// // Function to be used as a callback
-// function greetUser(name) {
-//   console.log(`Hello! ${name}`);
-// }
-
-// processUserInput("Vinod", greetUser);
-
-// processUserInput is a higher-order function because it takes another function (callback) as an argument.
-// greetUser is a callback function because it's passed as an argument to processUserInput and gets executed after the completion of the main task
-
-//* -------------------------------
-//*  Closure:
-//* -------------------------------
-
-//? A closure is created when an inner function has access to the variables of its outer function, even after the outer function has finished executing.
-
-// function multiplier(factor) {
-//   return function (number) {
-//     console.log(number, factor);
-//     return number * factor;
-//   };
-// }
-
-// const double = multiplier(2);
-// console.log(double(5));
-
-//* also one more example
-// function outerFunction() {
-//   var outerVariable = "I'm from outer";
-
-//   function innerFunction() {
-//     console.log(outerVariable);
-//   }
-
-//   return innerFunction;
-// }
-
-// var closureFunction = outerFunction();
-// closureFunction(); // Outputs: "I'm from outer"
-
-// Here when we are calling the closureFunction() it is actually going to call the innerFunction() but in our executionContext the outerFunction is already popped  out. Still we can access the outerVaribale value and how we are able to access it, is what closure all about ?
-
-// We just need to know that an inner function has always access to the variables of the outer (parent) function. That's it.
-
-// Why is it like that? It's because the variable object of the parent function stays in memory even after the function returns, and our inner function has access to this variable object through the scope chain.
-
-// It might be beneficial to clarify that the call stack is simply the execution order and it is not the same thing as the computer's memory, which is where the variables are held.  So even though the function was removed from the call stack, its variables are still maintained in memory until they are no longer needed and garbage collection comes and removes them.
-
-//* ===================================
-//* Interview Question:
-//* ===================================
-//! Write a program to perform mathematical operations using callback functions and two variables in JavaScript.
-
-//? Instructions:
-//? Define a higher-order function called mathOperation that takes three arguments: x, y, and operation.
-//? Implement two callback functions:
-//? add: Takes two numbers x and y and returns their sum.
-//? sub: Takes two numbers x and y and returns the result of subtracting x from y.
-//? Use the mathOperation function to perform addition and subtraction operations on two variables a and b.
-//? Display the results of the operations.
-
-const mathOperation = (a, b, operation) => {
-  return operation(a, b);
+// to get the actual country name
+const getCountryName = (code) => {
+  return new Intl.DisplayNames([code], { type: "region" }).of(code);
 };
 
-const add = (a, b) => {
-  return a + b;
+// to get the date and time
+const getDateTime = (dt) => {
+  const curDate = new Date(dt * 1000); // Convert seconds to milliseconds
+  console.log(curDate);
+  // // const date = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+
+  const formatter = new Intl.DateTimeFormat("en-US", options);
+  console.log(formatter);
+  return formatter.format(curDate);
 };
 
-const sub = (a, b) => {
-  return b - a;
+let city = "pune";
+
+// search functionality
+citySearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let cityName = document.querySelector(".city_name");
+  console.log(cityName.value);
+  city = cityName.value;
+
+  getWeatherData();
+
+  cityName.value = "";
+});
+
+const getWeatherData = async () => {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid={42c337395802cf64f1149c09c99cada6}`;
+  try {
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
+    console.log(data);
+
+    const { main, name, weather, wind, sys, dt } = data;
+
+    cityName.innerHTML = `${name}, ${getCountryName(sys.country)}`;
+    dateTime.innerHTML = getDateTime(dt);
+
+    w_forecast.innerHTML = weather[0].main;
+    w_icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather[0].icon}@4x.png" />`;
+
+    w_temperature.innerHTML = `${main.temp}&#176`;
+    w_minTem.innerHTML = `Min: ${main.temp_min.toFixed()}&#176`;
+    w_maxTem.innerHTML = `Min: ${main.temp_max.toFixed()}&#176`;
+
+    w_feelsLike.innerHTML = `${main.feels_like.toFixed(2)}&#176`;
+    w_humidity.innerHTML = `${main.humidity}%`;
+    w_wind.innerHTML = `${wind.speed} m/s`;
+    w_pressure.innerHTML = `${main.pressure} hPa`;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-console.log(mathOperation(5, 15, add));
-console.log(mathOperation(5, 15, sub));
+document.body.addEventListener("load", getWeatherData());
